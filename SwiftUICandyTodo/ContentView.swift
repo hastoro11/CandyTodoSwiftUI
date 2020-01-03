7f44894f5834767494589a36f9ea2f1ecf3bd4b9
Gabor Sornyei <info@sornyei.com>
2020-01-03 08:51:53 +0100

    Initial Commit

---
+++ SwiftUICandyTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICandyTodo
+//
+//  Created by Gabor Sornyei on 2020. 01. 03..
+//  Copyright © 2020. Gabor Sornyei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
